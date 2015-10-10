41ec151913bb7327981968bcb8eb8b653639de81
jeroendedauw <jeroendedauw@gmail.com>
2015-10-10 18:03:24 +0200

    Implement dec to arbitrary

--- src/number_converter/core.clj
+++ src/number_converter/core.clj
@@ -1,12 +1,21 @@
 (ns number-converter.core
   (:gen-class))
 
+(use 'clojure.math.numeric-tower)
+
 (defn -main
   [& args]
   (println "Hello, World!"))
 
 (defn convert-number
   ([n output-symbols]
-   (convert-number n (range 10) output-symbols))
-  ([n input-symbols output-symbols]
-   (str (nth output-symbols (.indexOf input-symbols n)))))
+   (loop [n n
+          step 1
+          acc '()]
+     (if (< n 1)
+       (if (= step 1) [(first output-symbols)] acc)
+       (let [symbol-value (rem n (expt (count output-symbols) step))]
+         (recur
+           (- n symbol-value)
+           (inc step)
+           (cons (nth output-symbols (/ symbol-value (expt (count output-symbols) (dec step)))) acc) ))))))
--- test/number_converter/core_test.clj
+++ test/number_converter/core_test.clj
@@ -4,12 +4,25 @@
 
 
 (deftest decimal-to-binary
-  (is (= "0" (convert-number 0 [0 1])))
-  (is (= "1" (convert-number 1 [0 1])))
+  (is (= ["0"] (convert-number 0 ["0" "1"])))
+  (is (= ["1"]  (convert-number 1 ["0" "1"])))
+  (is (= ["1" "0"] (convert-number 2 ["0" "1"])))
+  (is (= ["1" "1"] (convert-number 3 ["0" "1"])))
+  (is (= ["1" "0" "0"] (convert-number 4 ["0" "1"])))
+  (is (= ["1" "0" "1"] (convert-number 5 ["0" "1"])))
+  (is (= ["1" "1" "0"] (convert-number 6 ["0" "1"])))
+  (is (= ["1" "1" "1"] (convert-number 7 ["0" "1"])))
   )
 
 (deftest decimal-to-abc
-  (is (= "a" (convert-number 0 ["a" "b" "c"])))
-  (is (= "b" (convert-number 1 ["a" "b" "c"])))
+  (is (= ["a"] (convert-number 0 ["a" "b" "c"])))
+  (is (= ["b"] (convert-number 1 ["a" "b" "c"])))
+  (is (= ["c"] (convert-number 2 ["a" "b" "c"])))
+  (is (= ["b" "a"] (convert-number 3 ["a" "b" "c"])))
+  (is (= ["b" "b"] (convert-number 4 ["a" "b" "c"])))
+  (is (= ["b" "c"] (convert-number 5 ["a" "b" "c"])))
+  (is (= ["c" "a"] (convert-number 6 ["a" "b" "c"])))
+  (is (= ["c" "b"] (convert-number 7 ["a" "b" "c"])))
+  (is (= ["b" "a" "a"] (convert-number 9 ["a" "b" "c"])))
   )
 
