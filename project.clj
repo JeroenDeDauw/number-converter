9c4515fbc0c73e1870001f346a0976a500ba1827
jeroendedauw <jeroendedauw@gmail.com>
2015-10-12 17:34:21 +0200

    Add CLI interface

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "GNU GPL v2+"
             :url "http://www.gnu.de/documents/gpl-2.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :main ^:skip-aot number-converter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/number_converter/core.clj
+++ src/number_converter/core.clj
@@ -1,11 +1,9 @@
 (ns number-converter.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojure.string :as str])
   (:gen-class))
 
 (use 'clojure.math.numeric-tower)
-
-(defn -main
-  [& args]
-  (println "Hello, World!"))
 
 (defn decimal-to-number [n output-symbols]
   (loop [n n
@@ -32,6 +30,34 @@
 
 (defn convert-number
   ([n output-symbols]
-    (decimal-to-number n output-symbols))
+   (decimal-to-number n output-symbols))
   ([ns input-symbols output-symbols]
-    (convert-number (number-to-decimal ns input-symbols) output-symbols)))
+   (convert-number (number-to-decimal ns input-symbols) output-symbols)))
+
+(defn converter-number-input [ns input-symbols output-symbols]
+  (convert-number
+    (str/split ns #" ")
+    (str/split input-symbols #" ")
+    (str/split output-symbols #" ")))
+
+(def cli-options
+  ;; An option with a required argument
+  [["-n" "--number SYMBOLS" "Symbols of the number to convert"
+    ]
+   ["-i" "--input SYMBOLS" "Symbols defining the input system"
+    ]
+   ["-o" "--output SYMBOLS" "Symbols defining the output system"
+    ]
+   ["-h" "--help"]])
+
+(defn -main [& args]
+  (let [cli-input (parse-opts args cli-options)]
+    (if (nil? (cli-input :errors))
+      (let [options (cli-input :options)]
+        (if (true? (options :help))
+          (println (cli-input :summary))
+          (println (converter-number-input
+                     (options :number)
+                     (options :input)
+                     (options :output)))))
+      ((println (cli-input :errors)) (println (cli-input :summary))))))
